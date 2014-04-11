5a5cbf40de36df17d12b9bb38802685557c1f22a
lordsoony <165175546@qq.com>
2014-04-11 14:05:51 +0800

    js changed to custom tips for each record

--- all/themes/easyloan/templates/account/createinvestplan.tpl.php
+++ all/themes/easyloan/templates/account/createinvestplan.tpl.php
@@ -8,18 +8,25 @@ drupal_add_js(drupal_get_path('theme','easyloan') . '/js/account.js');
 drupal_add_js(drupal_get_path('theme','easyloan') . '/js/tab.js');
 global $base_url;
 ?>
-
 <script>
 (function ($, Drupal, window, document, undefined) {
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.tip = {
   attach: function(context, settings) {
-  $('.easytip').powerTip({ placement: 'ne' });
+    $('.easytip')
+      .powerTip({ 
+            placement: 'ne',    // display the tip to the northeast
+            mouseOnToPopup: 'true', // allow mouse to hover on the conent of tip
+          })
+      .data('powertip', function() {
+            // alert($(this).html()); // custom json data for each tipped element
+            var tipHtml = 'Hello';
+          return tipHtml;
+        });;
   }
 };
 })(jQuery, Drupal, this, this.document);
 </script>
-
 <div class="color-white-bg" id="withdrawappl">
   <div class="ui-tab ui-tab-transparent" id="withdrawappl-tab">
     <ul class="ui-tab-items">
@@ -69,7 +76,14 @@ Drupal.behaviors.tip = {
             <span class="ui-list-field w60 fn-left text-center">15%</span>
             <span class="ui-list-field w60 fn-left text-center easytip" title="借款日期: 2014-04-08<br />到期日期: 2015-10-08">18个月</span>
             <span class="ui-list-field w50 fn-left text-center easytip" title="单利">20%</span>
-            <span class="ui-list-field w90 fn-left text-center">2014-08-09</span>
+            <span class="ui-list-field w90 fn-left text-center easytip" title="
+                <li>This is a line</li>
+                <li>This is a line</li>
+                <li>This is a line</li>
+                <li>This is a line</li>
+                <li>This is a line</li>
+                <li>This is a line</li>
+            ">2014-08-09</span>
             <span class="ui-list-field w80 fn-left">
               <a class="ui-button ui-button-small ui-button-blue">不发布</a>
             </span>
@@ -99,10 +113,11 @@ Drupal.behaviors.tip = {
             <span class="ui-list-title w80 ph10 fn-left">借款标题</span>
             <span class="ui-list-title w60 fn-left">借款人</span>
             <span class="ui-list-title w100 fn-left">借款金额</span>
-            <span class="ui-list-title w60 fn-left">借款利息</span>
+            <span class="ui-list-title w50 fn-left two-line">借款利息</span>
             <span class="ui-list-title w60 fn-left">借款年利率</span>
             <span class="ui-list-title w60 fn-left">借款期限</span>
-            <span class="ui-list-title w80 fn-left">借款逾期日利率</span>
+            <span class="ui-list-title w60 fn-left two-line">借款逾期日利率</span>
+
             <span class="ui-list-title w100 fn-left">还清日期</span>
             <span class="ui-list-title w100 fn-left">借款逾期罚金</span>
             <span class="ui-list-title w40 fn-left">投资年利率</span>
@@ -125,10 +140,6 @@ Drupal.behaviors.tip = {
             <span class="ui-list-field w40 fn-left easytip" title="借款日期: 2014-04-08<br />到期日期: 2015-10-08">18个月</span>
             <span class="ui-list-field w60 fn-left easytip" title="单利">20%</span>
             <span class="ui-list-field w100 fn-left">2014-08-09</span>
-            <span class="ui-list-field w100 two-line fn-left">
-              <a class="ui-button ui-button-small ui-button-blue">不发布</a>
-              <a class="ui-button ui-button-small ui-button-green">立即发布</a>
-            </span>
           </li>
           <!--li class="ui-list-status">
             <p class="color-gray-text">没有记录</p>
