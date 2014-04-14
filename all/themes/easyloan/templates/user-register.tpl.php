db5d213f4bcd654c83935b7ef252a56b991a7bb9
songyang <waoywssy@gmail.com>
2014-04-14 17:27:35 +0800

    register form under theming 2014-04-14

--- all/themes/easyloan/templates/user-register.tpl.php
+++ all/themes/easyloan/templates/user-register.tpl.php
@@ -10,57 +10,70 @@
 drupal_add_js(drupal_get_path('theme','easyloan') . '/js/jquery.validate.min.js');
 drupal_add_js(drupal_get_path('theme','easyloan') . '/js/reg.js');
 drupal_add_css(drupal_get_path('theme','easyloan') . '/css/reg.css');
-/*
-print render($form['account']['name']);
-print render($form['account']['pass']);
-print render($form['account']['mail']);
-print render($form['captcha']);
-//print drupal_render($form['actions']);
-*/
-//var_dump($form);
+
+$form['captcha']['captcha_widgets']['captcha_response']['#title']='';
+$form['captcha']['captcha_widgets']['captcha_response']['#description']='';
+//$form['captcha']['captcha_widgets']['captcha_response']['#theme']='';
+//$form['captcha']['captcha_widgets']['captcha_response']['#theme_wrappers']='';
+// print render($form['actions']);
+
 ?>
 <div id="pg-reg">
   <div class="container_12">
     <div class="p20bs color-white-bg regbox">
-      <div data-name="register" data-showtip="false" class="ui-form" method="post" id="reg">
+      <div data-name="register" class="ui-form" id="reg">
         <fieldset>
+
+<?php 
+/*
+print render($form['account']['mail']);
+print render($form['account']['pass']);
+print render($form['captcha']);
+print render($form['account']['agree']);
+*/
+?>
           <legend>注册</legend>
           <div class="ui-form-item">
             <label class="ui-label">昵称</label>
-            <input class="ui-input input-icon" type="text" name="name" id="edit-name">
+            <?php print render($form['account']['name']);?>
             <span class="icon input-icon-user"></span>
           </div>
           <div class="ui-form-item">
             <label class="ui-label">手机号</label>
-            <input class="ui-input input-icon" type="text" name="phone" id="phone">
+            <?php print render($form['account']['phone']);?>
             <span class="icon input-icon-mobile"></span>
+            <input type="hidden" name="mail" id="edit-mail" value="13094810441@vip.com">
           </div>
           <div class="ui-form-item">
             <label class="ui-label">密码</label>
-            <input class="ui-input input-icon" type="password" name="password" id="password">
+            <input class="ui-input input-icon" type="password" 
+              name="<?php print $form['account']['pass']['pass1']['#name']?>" id="<?php print $form['account']['pass']['pass1']['#id']?>">
             <span class="icon input-icon-key"></span>
           </div>
           <div class="ui-form-item">
             <label class="ui-label">重复密码</label>
-            <input class="ui-input input-icon" type="password" name="confirm_password" onpaste="return false">
+            <input class="ui-input input-icon" type="password" 
+              name="<?php print $form['account']['pass']['pass2']['#name']?>" id="<?php print $form['account']['pass']['pass2']['#id']?>">
             <span class="icon input-icon-key"></span>
           </div>
           <div class="ui-form-item">
             <label class="ui-label">验证码</label>
-            <input class="ui-input input-icon code" type="text" name="randCode" id="randCode">
+            <input class="ui-input input-icon code" type="text" name="captcha_response" id="edit-captcha-response">
+            <?php print render($form['captcha']);?>
             <span class="icon input-icon-lock"></span>
-            <img id="randImage" border="0" align="absmiddle" src="https://www.renrendai.com/image.jsp" name="randImage" alt="验证码">
           </div>
+          <?php 
+            print render($form['form_build_id']);
+            print render($form['form_id']);
+            print render($form['account']['timezone']);
+          ?>
           <div class="ui-form-item ui-form-item-check">
-            <input id="agree" name="agree" checked="checked" type="checkbox"> <label id="labelagree" for="agree">我已阅读并同意  <a href="/agreement/rv_webservice.html" target="_blank">《好易贷网站服务协议》</a> </label>
+            <input id="<?php print $form['account']['agree']['#id']; ?>" name="<?php print $form['account']['agree']['#name']; ?>" 
+              checked="checked" type="checkbox"> <label id="labelagree" for="<?php print $form['account']['agree']['#id']; ?>">我已阅读并同意  <a href="/agreement/rv_webservice.html" target="_blank">《好易贷网站服务协议》</a> </label>
           </div>
           <div class="ui-form-item">
-            <input type="submit" id="edit-submit" name="op" class="ui-button ui-button-blue ui-button-mid" value="注册">
+            <?php print render($form['actions']['submit']);?>
           </div>
-              <?php 
-                print drupal_render($form['form_build_id']);
-                print drupal_render($form['form_id']);
-              ?>
         </fieldset>
       </div>
     </div>
