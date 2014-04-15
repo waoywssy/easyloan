e8bb0fb4e48683812fd982f771e80d156a0a1f9c
songyang <waoywssy@gmail.com>
2014-04-15 17:15:17 +0800

    register form succeed, ready to validate

--- all/themes/easyloan/js/reg.js
+++ all/themes/easyloan/js/reg.js
@@ -1,54 +1,74 @@
-(function ($) {
-  // All your code here
-  $("#user-register-form").validate({
-		rules: {
-			phone:"required"/*{
-				required: true,
-				minlength: 2,
-			},
-			phone: {
-				required: true,
-				minlength: 11,
-				maxlength: 11
-			},
-			password: {
-				required: true,
-				minlength: 6
-			},
-			confirm_password: {
-				required: true,
-				minlength: 6,
-				equalTo: "#password"
-			},
-			agree: "required"*/
-		},
-		messages:{
-			phone:{
-				required: "Hello",
-				minlength: "Name"
-			},
-		}
-		/*
-		messages: {
-			nickName: {
-				required: "2-20位字符，可包含中文，英文，数字和字符\"-\"与\"_\"。注册完成后不可修改",
-				minlength: "注册名需包含至少两个字符"
-			},
-			phone: {
-				required: "请填入11位数字",
-				minlength: "请正确填入手机号码",
-				minlength: "请正确填入手机号码",
-			},
-			password: {
-				required: "密码须为6-16位英文字母、数字和符号(不包括空格)",
-				minlength: "密码至少为6个字符",
-			},
-			confirm_password: {
-				required: "请重复输入密码",
-				minlength: "密码至少为6个字符",
-				equalTo: "请重复输入密码"
-			},
-			agree: "请同意我们的条款"
-		}*/
-	});
-})(jQuery);
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.reg = {
+  attach: function(context, settings) {
+
+		  // All your code here
+		  $("#user-register-form").validate({
+				rules: {
+					edit-phone:"required"/*{
+						required: true,
+						minlength: 2,
+					},
+					phone: {
+						required: true,
+						minlength: 11,
+						maxlength: 11
+					},
+					password: {
+						required: true,
+						minlength: 6
+					},
+					confirm_password: {
+						required: true,
+						minlength: 6,
+						equalTo: "#password"
+					},
+					agree: "required"*/
+				},
+				messages:{
+					edit-phone:{
+						required: "Hello",
+						minlength: "Name",
+					},
+				}
+				/*
+				messages: {
+					nickName: {
+						required: "2-20位字符，可包含中文，英文，数字和字符\"-\"与\"_\"。注册完成后不可修改",
+						minlength: "注册名需包含至少两个字符"
+					},
+					phone: {
+						required: "请填入11位数字",
+						minlength: "请正确填入手机号码",
+						minlength: "请正确填入手机号码",
+					},
+					password: {
+						required: "密码须为6-16位英文字母、数字和符号(不包括空格)",
+						minlength: "密码至少为6个字符",
+					},
+					confirm_password: {
+						required: "请重复输入密码",
+						minlength: "密码至少为6个字符",
+						equalTo: "请重复输入密码"
+					},
+					agree: "请同意我们的条款"
+				}*/
+			});
+  }
+};
+})(jQuery, Drupal, this, this.document);
--- all/themes/easyloan/templates/user-register.tpl.php
+++ all/themes/easyloan/templates/user-register.tpl.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * @file
- * Returns the HTML for a node.
+ * Returns the HTML for a the user-register form.
  *
- * Complete documentation for this file is available online.
- * @see https://drupal.org/node/1728164
  */
 
 drupal_add_js(drupal_get_path('theme','easyloan') . '/js/jquery.validate.min.js');
@@ -12,67 +10,70 @@ drupal_add_js(drupal_get_path('theme','easyloan') . '/js/reg.js');
 drupal_add_css(drupal_get_path('theme','easyloan') . '/css/reg.css');
 
 $form['captcha']['captcha_widgets']['captcha_response']['#title']='';
+$form['captcha']['captcha_widgets']['captcha_response']['#weight']=-10;
 $form['captcha']['captcha_widgets']['captcha_response']['#description']='';
-//$form['captcha']['captcha_widgets']['captcha_response']['#theme']='';
-//$form['captcha']['captcha_widgets']['captcha_response']['#theme_wrappers']='';
-// print render($form['actions']);
-
+$form['captcha']['captcha_widgets']['captcha_response']['#required']=false;
+$form['captcha']['captcha_widgets']['captcha_refresh']['#theme_wrappers']=NULL;
+$form['captcha']['captcha_widgets']['captcha_response']['#attributes']['class']=array('ui-input','input-icon','code');
 ?>
-<div id="pg-reg">
-  <div class="container_12">
-    <div class="p20bs color-white-bg regbox">
-      <div data-name="register" class="ui-form" id="reg">
-        <fieldset>
 
-<?php 
-/*
-print render($form['account']['mail']);
-print render($form['account']['pass']);
-print render($form['captcha']);
-print render($form['account']['agree']);
-*/
-?>
-          <legend>注册</legend>
-          <div class="ui-form-item">
-            <label class="ui-label">昵称</label>
-            <?php print render($form['account']['name']);?>
-            <span class="icon input-icon-user"></span>
-          </div>
-          <div class="ui-form-item">
-            <label class="ui-label">手机号</label>
-            <?php print render($form['account']['phone']);?>
-            <span class="icon input-icon-mobile"></span>
-            <input type="hidden" name="mail" id="edit-mail" value="13094810441@vip.com">
-          </div>
-          <div class="ui-form-item">
-            <label class="ui-label">密码</label>
-            <input class="ui-input input-icon" type="password" 
-              name="<?php print $form['account']['pass']['pass1']['#name']?>" id="<?php print $form['account']['pass']['pass1']['#id']?>">
-            <span class="icon input-icon-key"></span>
-          </div>
-          <div class="ui-form-item">
-            <label class="ui-label">重复密码</label>
-            <input class="ui-input input-icon" type="password" 
-              name="<?php print $form['account']['pass']['pass2']['#name']?>" id="<?php print $form['account']['pass']['pass2']['#id']?>">
-            <span class="icon input-icon-key"></span>
-          </div>
-          <div class="ui-form-item">
-            <label class="ui-label">验证码</label>
-            <input class="ui-input input-icon code" type="text" name="captcha_response" id="edit-captcha-response">
-            <?php print render($form['captcha']);?>
-            <span class="icon input-icon-lock"></span>
-          </div>
+<div id="pg-reg"> 
+  <div class="container_12"> 
+    <div class="p20bs color-white-bg regbox"> 
+      <div data-name="register" class="ui-form" id="reg"> 
+        <fieldset> 
           <?php 
             print render($form['form_build_id']);
             print render($form['form_id']);
-            print render($form['account']['timezone']);
           ?>
+          <legend>注册</legend>
+          <div class="ui-form-item">
+            <label class="ui-label">昵称</label>
+            <?php print render($form['account']['name']); ?>
+            <span class="icon input-icon-user"></span>
+          </div>
+          
+          <div class="ui-form-item">
+            <label class="ui-label">手机号</label>
+            <?php print render($form['account']['phone']); ?>
+            <span class="icon input-icon-mobile"></span>
+            <input type="hidden" name="mail" id="edit-mail" value="@">
+          </div>
+
+          <div class="ui-form-item">
+            <label class="ui-label">密码</label>
+            <input class="ui-input input-icon" type="password" 
+              name="<?php print $form['account']['pass']['pass1']['#name']; ?>" id="<?php print $form['account']['pass']['pass1']['#id'];?>">
+            <span class="icon input-icon-key"></span>
+          </div>
+
+          <div class="ui-form-item">
+            <label class="ui-label">重复密码</label>
+            <input class="ui-input input-icon" type="password" 
+              name="<?php print $form['account']['pass']['pass2']['#name']; ?>" id="<?php print $form['account']['pass']['pass2']['#id'];?>">
+            <span class="icon input-icon-key"></span>
+          </div>
+          <?php 
+            if($form['captcha']['#captcha_type']){
+              print render($form['captcha']['captcha_sid']);
+              print render($form['captcha']['captcha_token']);
+          ?>
+          <div class="ui-form-item">
+            <label class="ui-label">验证码</label>
+            <?php print render($form['captcha']); ?>
+            <span class="icon input-icon-lock"></span>
+          </div>
+          
+          <?php } ?>
           <div class="ui-form-item ui-form-item-check">
-            <input id="<?php print $form['account']['agree']['#id']; ?>" name="<?php print $form['account']['agree']['#name']; ?>" 
-              checked="checked" type="checkbox"> <label id="labelagree" for="<?php print $form['account']['agree']['#id']; ?>">我已阅读并同意  <a href="/agreement/rv_webservice.html" target="_blank">《好易贷网站服务协议》</a> </label>
+            <?php print render($form['account']['agree']); ?>
+            <a href="/agreement/rv_webservice.html" target="_blank">《好易贷网站服务协议》</a>
           </div>
           <div class="ui-form-item">
-            <?php print render($form['actions']['submit']);?>
+            <?php 
+              print render($form['account']['timezone']);
+              print render($form['actions']['submit']); 
+            ?>
           </div>
         </fieldset>
       </div>
